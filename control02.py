4bc8ec784e1118caf320242f78c8579ff903f34a
wnsdyd0612 <54461186+wnsdyd0612@users.noreply.github.com>
2019-09-21 12:07:14 +0900

    Add files via upload

---
+++ control02.py
@@ -0,0 +1,5 @@
+dan = int(input("구구단 입력 : "))
+i = 1
+while i < 10:
+    print("{} x {} = {}\n".format(dan, i, (dan * i)))
+    i = i + 1
---
+++ control03.py
@@ -0,0 +1,5 @@
+add = 0
+for i in range(1,101):#1부터 101까지의 합
+    add = add + i
+    
+print(add)
